6329ab29a44107f18af49d5ca849660a9c71f60b
luis antonio amaya villalta <amayantonio@hotmail.com>
2021-04-21 14:51:47 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Estado = "";
+            bool RedActiva = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+
+            if (RedActiva)
+                Estado = "Se estableció conexión con la red local";
+            else
+                Estado = "No está conectado a la red";
+            Console.Write(Estado);
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
